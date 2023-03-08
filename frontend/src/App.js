edc214a7f3202ce7842d0507bed2abc5275aab12
KrzysztofHum <k.humienny1996@protonmail.com>
2023-03-08 10:57:39 +0100

    forgotpassword, login, signup, resetpassword, privacypolicy pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,11 @@ import About from "./pages/About";
 import OurStore from "./pages/OurStore";
 import Blog from "./pages/Blog";
 import CompareProduct from "./pages/CompareProduct";
+import Forgotpassword from "./pages/Forgotpassword";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Resetpassword from "./pages/Resetpassword";
 
 function App() {
   return (
@@ -19,6 +24,11 @@ function App() {
           <Route path="product" element={<OurStore />} />
           <Route path="blogs" element={<Blog />} />
           <Route path="compare-product" element={<CompareProduct />} />
+          <Route path="login" element={<Login />} />
+          <Route path="forgot-password" element={<Forgotpassword />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="reset-password" element={<Resetpassword />} />
+          <Route path="privacy-policy" element={<PrivacyPolicy />} />
         </Route>
       </Routes>
     </BrowserRouter>
