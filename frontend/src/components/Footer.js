689875c53170c89384be0319ffb4a218f22b8fce
KrzysztofHum <k.humienny1996@protonmail.com>
2023-03-08 08:10:55 +0100

    contact page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -37,21 +37,22 @@ const Footer = () => {
               <h4 className="text-white mb-4">Contact Us</h4>
               <div>
                 <address className="text-white fs-6">
-                  ul. Świętojańska 33/1,
-                  <br /> Gdynia, Pomorskie
-                  <br /> 11-222
+                  ul. Świętokrzyska 3/2,
+                  <br />
+                  Gdynia 81-582,
+                  <br /> Pomorskie
                 </address>
                 <a
                   href="tel: +48 237953123"
                   className="mt-3 d-block mb-1 text-white"
                 >
-                  +48 237953123
+                  +48 689323455
                 </a>
                 <a
                   href="mailto: mediaking3232@gmail.com"
                   className="mt-2 d-block mb-0 text-white"
                 >
-                  mediaking3232@gmail.com
+                  mediaking@gmail.com
                 </a>
                 <div className="social_icons d-flex align-items-center gap-30 mt-4">
                   <a href="/" className="text-white">
--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,8 +1,102 @@
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
+import Container from "../components/Container";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <Meta title={"Contact Us"} />
+      <BreadCrumb title="Contact Us" />
+      <Container class1="contact-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <iframe
+              title="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27008.322846771294!2d18.474810810541033!3d54.458321335487405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fda055301e9697%3A0x3327466aa1187822!2sWielki%20Kack%2C%20Gdynia!5e0!3m2!1spl!2spl!4v1678258555677!5m2!1spl!2spl"
+              width="600"
+              height="450"
+              className="border-0 w-100"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div className="col-12 mt-5">
+            <div className="contact-inner-wrapper d-flex justify-content-between ">
+              <div>
+                <h3 className="contact-title mb-4">Contact</h3>
+                <form action="" className="d-flex flex-column gap-15">
+                  <div>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="tel"
+                      className="form-control"
+                      placeholder="Mobile Number"
+                    />
+                  </div>
+                  <div>
+                    <textarea
+                      name=""
+                      id=""
+                      className="w-100 form-control"
+                      cols="30"
+                      rows="4"
+                      placeholder="Comments"
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button className="button border-0">Submit</button>
+                  </div>
+                </form>
+              </div>
+              <div>
+                <h3 className="contact-title mb-4">Get in touch with us</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineHome className="fs-5" />
+                      <address className="mb-0">
+                        ul. Świętokrzyska 3/2, Gdynia 81-582
+                      </address>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiPhoneCall className="fs-5" />
+                      <a href="tel:+91 8264954234">+48 689323455</a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineMail className="fs-5" />
+                      <a href="mailto:navdeepdahiya753@gmail.com">
+                        mediaking@gmail.com
+                      </a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiInfoCircle className="fs-5" />
+                      <p className="mb-0">Monday – Friday 8 AM – 8 PM</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
